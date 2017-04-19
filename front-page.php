60c4e12c85f82702656203ded7c2c4bd7286d3ed
Mary Bantigue <Mary Bantigue>
2017-04-20 04:58:23 +0800

    add new files

---
+++ front-page.php
@@ -0,0 +1,69 @@
+<?php
+get_header(); ?>
+
+<div class="container banner">
+    <div class="banner-content">
+        <h1>Do you want your students to think about environmental issues?</h1>
+        <a href="" class="btn-banner">FIND OUT HOW</a>
+    </div>
+    
+</div>
+<section class="home-content">
+    <div class="row">
+        <div class="col-md-6 news-wrap">
+            <h2>What’s Happening?</h2>
+
+            <h5>On Line Courses available Spring 2017</h5>
+            <p>The Ecological Restoration Training Cooperative welcomes everyone interested in restoring the quality, diversity, and productivity of our ecosystems.</p>
+            <h5>2017 Seedlings Now Available!</h5>
+            <p>We are now accepting your 2017 Seedling Orders. New this year - Acute Willows and Golden Willows. Don’t wait until last minute to order. Call, Fax, Phone or place your order on-line. </p>
+
+            <hr />
+            <h2>Get Involved</h2>
+
+        </div>
+        <div class="col-md-6">
+
+        </div>
+    </div>
+</section>
+<section class="thinking-about-trees text-center">
+    <div class="container">
+        <h2>Are you thinking about trees?</h2>
+        <div class="row">
+            <div class="col-md-3">
+                <h6>Seedlings and Tree Sales</h6>
+                <img src="img/seedlings_icon.png" />
+            </div>
+            <div class="col-md-3">
+                <h6>Wedding Favours </h6>
+                <img src="img/weddingfavors_icon.png" />
+            </div>
+            <div class="col-md-3">
+                <h6>Special Event Seedling</h6>
+                <img src="img/specialevents_icon.png" />
+            </div>
+            <div class="col-md-3">
+                <h6>Heritage Trees</h6>
+                <img src="img/heritagetree_icon.png" />
+            </div>
+        </div>
+
+    </div>
+
+</section>
+<section class="make-donation-wrap">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                <h2>Make a Donation</h2>
+                <p>Donating to Manitoba Forestry Association takes minutes, and it’s thanks to your support that we plant trees, and enhance the overall capacity and success of Manitoba’s tree planting efforts.</p>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class="btn-donate">DONATE NOW!</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer();
